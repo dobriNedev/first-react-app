21aa441608b93ead2d6672a7c09c9f16b9005647
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-23 12:38:00 +0100

    first-react-App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import EventContainer from "./components/eventContainer";
+import EventShedule from "./components/EventShedule";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Nav from "./components/Navigation";
+import Tickets from "./components/Tickets";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      <Header />
+      <EventContainer />
+      <Tickets />
+      <EventShedule />
+      <Footer />
     </div>
+    
   );
 }
 
